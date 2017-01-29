1083b660db0049fc1fe4691299a04799c82ccf73
Vaughan Kitchen <v.kitchen@gnoms.org>
2017-01-29 16:47:11 +1300

    all opcodes and debug values

--- source/disassemble.c
+++ source/disassemble.c
@@ -24,69 +24,146 @@ struct string *disassemble(struct string *bytecode)
 
 	for (pos = 0; pos < len; pos++)
 		{
-		if ((code[pos] & 15 << 12) == 0)
+		printf("CODE: %d\n", code[pos]);
+		if ((code[pos] & (15 << 12 | 15 << 8 | 15)) == 0)
 			{
-			string_append_c(output, "CLS RET SYS\n");
+			string_append_c(output, "CLS\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15 << 8)) == 0)
+			{
+			string_append_c(output, "RET\n");
+			}
+		else if ((code[pos] & 15 << 12) == 0)
+			{
+			string_append_c(output, "SYS addr\n");
 			}
 		else if ((code[pos] & 15 << 12) == 1 << 12)
 			{
-			string_append_c(output, "JP\n");
+			string_append_c(output, "JP addr\n");
 			}
 		else if ((code[pos] & 15 << 12) == 2 << 12)
 			{
-			string_append_c(output, "CALL\n");
+			string_append_c(output, "CALL addr\n");
 			}
 		else if ((code[pos] & 15 << 12) == 3 << 12)
 			{
-			string_append_c(output, "SE\n");
+			string_append_c(output, "SE Vx, byte\n");
 			}
 		else if ((code[pos] & 15 << 12) == 4 << 12)
 			{
-			string_append_c(output, "SNE\n");
+			string_append_c(output, "SNE Vx, byte\n");
 			}
-		else if ((code[pos] & 15 << 12) == 5 << 12)
+		else if ((code[pos] & (15 << 12 | 15)) == 5 << 12)
 			{
-			string_append_c(output, "SE\n");
+			string_append_c(output, "SE Vx, Vy\n");
 			}
 		else if ((code[pos] & 15 << 12) == 6 << 12)
 			{
-			string_append_c(output, "LD\n");
+			string_append_c(output, "LD Vx, byte\n");
 			}
 		else if ((code[pos] & 15 << 12) == 7 << 12)
 			{
-			string_append_c(output, "ADD\n");
+			string_append_c(output, "ADD Vx, byte\n");
 			}
-		else if ((code[pos] & 15 << 12) == 8 << 12)
+		else if ((code[pos] & (15 << 12 | 15)) == 8 << 12)
 			{
-			string_append_c(output, "LD OR AND XOR ADD SUB SHR SUBN SHL\n");
+			string_append_c(output, "LD Vx, Vy\n");
 			}
-		else if ((code[pos] & 15 << 12) == 9 << 12)
+		else if ((code[pos] & (15 << 12 | 15)) == (8 << 12 | 1))
 			{
-			string_append_c(output, "SNE\n");
+			string_append_c(output, "OR Vx, Vy\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15)) == (8 << 12 | 2))
+			{
+			string_append_c(output, "AND Vx, Vy\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15)) == (8 << 12 | 3))
+			{
+			string_append_c(output, "XOR Vx, Vy\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15)) == (8 << 12 | 4))
+			{
+			string_append_c(output, "ADD Vx, Vy\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15)) == (8 << 12 | 5))
+			{
+			string_append_c(output, "SUB Vx, Vy\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15)) == (8 << 12 | 6))
+			{
+			string_append_c(output, "SHR Vx {, Vy}\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15)) == (8 << 12 | 7))
+			{
+			string_append_c(output, "SUBN Vx, Vy\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15)) == (8 << 12 | 14))
+			{
+			string_append_c(output, "SHL Vx {, Vy}\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15)) == 9 << 12)
+			{
+			string_append_c(output, "SNE Vx, Vy\n");
 			}
 		else if ((code[pos] & 15 << 12) == 10 << 12)
 			{
-			string_append_c(output, "LD\n");
+			string_append_c(output, "LD I, addr\n");
 			}
 		else if ((code[pos] & 15 << 12) == 11 << 12)
 			{
-			string_append_c(output, "JP\n");
+			string_append_c(output, "JP V0, addr\n");
 			}
 		else if ((code[pos] & 15 << 12) == 12 << 12)
 			{
-			string_append_c(output, "RND\n");
+			string_append_c(output, "RND Vx, byte\n");
 			}
 		else if ((code[pos] & 15 << 12) == 13 << 12)
 			{
-			string_append_c(output, "DRW\n");
+			string_append_c(output, "DRW Vx, Vy, nibble\n");
 			}
-		else if ((code[pos] & 15 << 12) == 14 << 12)
+		else if ((code[pos] & (15 << 12 | 15 << 4 | 15)) == (14 << 12 | 9 << 4 | 14))
 			{
-			string_append_c(output, "SKP SKNP\n");
+			string_append_c(output, "SKP Vx\n");
 			}
-		else if ((code[pos] & 15 << 12) == 15 << 12)
+		else if ((code[pos] & (15 << 12 | 15 << 4 | 15)) == (14 << 12 | 10 << 4 | 1))
 			{
-			string_append_c(output, "LD ADD\n");
+			string_append_c(output, "SKNP Vx\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15 << 4 | 15)) == (15 << 12 | 7))
+			{
+			string_append_c(output, "LD Vx, DT\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15 << 4 | 15)) == (15 << 12 | 10))
+			{
+			string_append_c(output, "LD Vx, K\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15 << 4 | 15)) == (15 << 12 | 1 << 4 | 5))
+			{
+			string_append_c(output, "LD DT, Vx\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15 << 4 | 15)) == (15 << 12 | 1 << 4 | 8))
+			{
+			string_append_c(output, "LD ST, Vx\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15 << 4 | 15)) == (15 << 12 | 1 << 4 | 14))
+			{
+			string_append_c(output, "ADD I, Vx\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15 << 4 | 15)) == (15 << 12 | 2 << 4 | 9))
+			{
+			string_append_c(output, "LD F, Vx\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15 << 4 | 15)) == (15 << 12 | 3 << 4 | 3))
+			{
+			string_append_c(output, "LD B, Vx\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15 << 4 | 15)) == (15 << 12 | 5 << 4 | 5))
+			{
+			string_append_c(output, "LD [I], Vx\n");
+			}
+		else if ((code[pos] & (15 << 12 | 15 << 4 | 15)) == (15 << 12 | 6 << 4 | 5))
+			{
+			string_append_c(output, "LD Vx, [I]\n");
 			}
 		else
 			{
